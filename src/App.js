570275cf01e80dce04a6d7502a76eacf263b8b7f
tiianyu123 <tiianyu123@qq.com>
2022-12-01 21:55:26 +0800

    基本框架

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Button} from 'antd'
+import './App.css'
+import Admin from "./admin";
+function App() {
+  return (
+    <div >
+      <Admin></Admin>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavLeft/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class index extends Component {
+  render() {
+    return (
+      <div>
+           <div className='logo'>  
+            <img src='' alt=''/>
+            <h1>mooc MS</h1>
+           </div>
+      </div>
+    )
+  }
+}
